a099299c6435bc9564b1f4eed06d556c6ff3a6cc
Robson da Silva Filho <robson.filho.ext@einstein.br>
2025-11-22 10:44:41 -0300

    fix: adjust new step

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -45,7 +45,7 @@ export default function CoupleProfile() {
     }
   }, [id]);
 
-  const [stage, setStage] = useState(1);
+  const [stage, setStage] = useState(0);
 
   enum GiftStage {
     GetYourGift = 0,
